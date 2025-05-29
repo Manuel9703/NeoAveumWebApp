f867e58aa8e9d7e162abc2aba3bce2369aa3566b
Manuel0301 <rasomanuel1@gmail.com>
2025-05-29 22:03:13 +0200

    test2

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/test', function (){
+   return  "pagina di test";
+})->name('test');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
